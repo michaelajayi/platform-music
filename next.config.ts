2492b4382bfc14b9a9d4ce100605421321dae28b
MICHAEL AJAYI O. <ajayimichael@hotmail.com>
2025-07-07 10:52:44 +0100

    Comment out experimental optimizeCss config

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    optimizeCss: true,
-  }
+  // experimental: {
+  //   optimizeCss: true,
+  // }
 };
 
 export default nextConfig;
